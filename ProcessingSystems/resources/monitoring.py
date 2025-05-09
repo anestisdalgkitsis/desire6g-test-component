35de7ff4678943cc89c505da0cd963bfd3328309
Anestis Dalgkitsis <anestisdalgkitsis@gmail.com>
2025-05-09 13:39:47 +0200

    Monitoring script

---
+++ ProcessingSystems/resources/monitoring.py
@@ -0,0 +1,15 @@
+# Mock Infrastructure Monitoring for development
+import time
+import logging
+
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
+
+def monitor_resources():
+    pass
+
+def monitor_performance():
+    pass
+
+def monitor_availability():
+    pass
